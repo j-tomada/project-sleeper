a2ce1c91e124466086ea95cb19c687cca3a285a1
JosephTomada <josephtomada@gmail.com>
2022-09-19 00:12:25 -0700

    created a search bar to fetch league roster from sleeper API

--- src/components/SearchLeague.js
+++ src/components/SearchLeague.js
@@ -1,1 +1,41 @@
 import React, { useState } from 'react';
+
+
+export default function SearchLeague ({ placeholder }) {
+    const [leagueID, setLeagueID] = useState("")
+
+    const HandleLeagueID = (event) => {
+        const input = event.target.value
+        if (input != leagueID) {
+            setLeagueID(input)
+            console.log(leagueID);
+        }
+    }
+    
+    function ConnectToLeague() {
+        fetch(`https://api.sleeper.app/v1/league/${leagueID}/rosters`).then((response) => {
+            if (response.ok) {
+                console.log(response.json())
+            }
+            else {
+                throw 'Error getting league'
+            }
+        })
+    }
+
+
+    return (
+        <div className='searchLeague'>
+            <input type='text'
+            placeholder={placeholder}
+            value={leagueID}
+            onChange={HandleLeagueID}
+            className='leagueSearchBar'
+            />
+            <button onClick={ConnectToLeague}>
+                Search League
+            </button>
+
+        </div>
+    );
+}
